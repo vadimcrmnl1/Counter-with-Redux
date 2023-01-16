b473b2e80e3bcc3645321645e9254433d045a27e
vadimcrmnl1 <you@example.com>
2023-01-16 20:00:27 +0300

    counter-with-redux 5 with redux local storage

--- src/store/store.ts
+++ src/store/store.ts
@@ -2,11 +2,32 @@ import {combineReducers, legacy_createStore} from 'redux';
 import {counterReducer} from "../reducers/counter-reducer";
 
 
+function saveToLocalStorage (state : AppRootStateType) {
+    try {
+        const serialisedState = JSON.stringify(state)
+        localStorage.setItem('persistantState', serialisedState)
+    } catch (e) {
+        console.warn(e)
+    }
+}
+
+function loadFromLocalStorage () {
+    try {
+        const serialisedState = localStorage.getItem('persistantState')
+        if (serialisedState === null) return undefined
+        return JSON.parse(serialisedState)
+    } catch (e) {
+        console.warn(e)
+        return undefined
+    }
+}
+
 export const rootReducer = combineReducers({
     counter: counterReducer
 })
 
-export const store = legacy_createStore(rootReducer)
+export const store = legacy_createStore(rootReducer, loadFromLocalStorage())
+store.subscribe(() => saveToLocalStorage(store.getState()))
 
 export type AppRootStateType = ReturnType<typeof rootReducer>
 
